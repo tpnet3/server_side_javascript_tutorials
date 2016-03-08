1f6108da5903ef4f8f13032576be00fd4a669f35
study <study@egoing-ui-MacBook-Air.local>
2016-03-08 21:27:03 +0900

    웹페이지를 표시하는 방법 취소

--- app.js
+++ app.js
@@ -4,22 +4,6 @@ app.use(express.static('public'));
 app.get('/', function(req, res){
     res.send('Hello home page');;
 });
-app.get('/dynamic', function(req, res){
-  var lis = '';
-  for(var i=0; i<5; i++){
-    lis = lis+'<li>coding</li>';
-  }
-  var output = `
-    <!DOCTYPE html>
-    <html>
-      <body>
-        Hello, Dynamic HTML
-        ${lis}
-      </body>
-    </html>
-  `;
-  res.send(output);
-});
 app.get('/route', function(req, res){
     res.send('Hello Router, <img src="/route.png">')
 })
