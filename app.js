39d47ed3ffc41a5122ad61ca5c1d625712e42d66
study <study@egoing-ui-MacBook-Air.local>
2016-03-08 21:51:35 +0900

    웹페이지를 서비스하는 방법0

--- app.js
+++ app.js
@@ -4,6 +4,29 @@ app.use(express.static('public'));
 app.get('/', function(req, res){
     res.send('Hello home page');;
 });
+app.get('/dynamic', function(req, res){
+  var lis = '';
+  for(var i=0; i<5; i++){
+    lis = lis + '<li>coding</li>';
+  }
+  var time = Date();
+  var output = `
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="utf-8">
+      <title></title>
+    </head>
+    <body>
+        Hello, Dynamic!
+        <ul>
+          ${lis}
+        </ul>
+        ${time}
+    </body>
+  </html>`;
+  res.send(output);
+});
 app.get('/route', function(req, res){
     res.send('Hello Router, <img src="/route.png">')
 })
