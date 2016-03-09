7e08fb8d61f537b71a588e95d4be10ab9017e185
study <study@egoing-ui-MacBook-Air.local>
2016-03-09 16:00:46 +0900

    query string 적용

--- app.js
+++ app.js
@@ -4,6 +4,23 @@ app.locals.pretty = true;
 app.set('view engine', 'jade');
 app.set('views', './views');
 app.use(express.static('public'));
+app.get('/topic', function(req, res){
+  var topics = [
+    'Javascript is....',
+    'Nodejs is...',
+    'Express is...'
+  ];
+  var output = `
+  <a href="/topic?id=0">JavaScript</a><br>
+  <a href="/topic?id=1">Nodejs</a><br>
+  <a href="/topic?id=2">Express</a><br><br>
+  ${topics[req.query.id]}
+  `
+  res.send(output);
+})
+app.get('/param/:module_id/:topic_id', function(req, res){
+  res.json(req.params);
+})
 app.get('/template', function(req, res){
   res.render('temp', {time:Date(), title:'Jade'});
 })
