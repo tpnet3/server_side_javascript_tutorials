ca6db1a592ee22d95dc6363da6b16922857888b1
egoing <egoing@gmail.com>
2016-03-10 14:21:37 +0900

    POST 방식을 이용한 정보의 전달 예제

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 app.locals.pretty = true;
 app.set('view engine', 'jade');
 app.set('views', './views');
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }))
 app.get('/form', function(req, res){
   res.render('form');
 });
 app.get('/form_receiver', function(req, res){
   var title = req.query.title;
   var description = req.query.description;
+  res.send(title+','+description);
+});
+app.post('/form_receiver', function(req, res){
+  var title = req.body.title;
+  var description = req.body.description;
   res.send(title+','+description);
 });
 app.get('/topic/:id', function(req, res){
