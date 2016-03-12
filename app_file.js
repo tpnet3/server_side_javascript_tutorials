2fabce8a730dc3d82e0bd0c005d3fda0bca9b7e8
egoing <egoing@gmail.com>
2016-03-12 17:26:13 +0900

    파일업로드4 - multer 심화

--- app_file.js
+++ app_file.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var multer = require('multer');
-var upload = multer({ dest: 'uploads/' })
+var _storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+})
+var upload = multer({ storage: _storage })
 var fs = require('fs');
 var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -12,7 +20,6 @@ app.get('/upload', function(req, res){
   res.render('upload');
 });
 app.post('/upload', upload.single('userfile'), function(req, res){
-  console.log(req.file);
   res.send('Uploaded : '+req.file.filename);
 });
 app.get('/topic/new', function(req, res){
