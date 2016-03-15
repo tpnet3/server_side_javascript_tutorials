32f78e7ef87d680e217ded4cd95ee5772737b9f7
egoing <egoing@gmail.com>
2016-03-16 02:47:59 +0900

    데이터베이스 orientdb9 웹앱제작 읽기2 상세보기 예제

---
+++ app_orientdb.js
@@ -0,0 +1,92 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var _storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+})
+var upload = multer({ storage: _storage });
+var fs = require('fs');
+var OrientDB = require('orientjs');
+var server = OrientDB({
+  host: 'localhost',
+  port: 2424,
+  username: 'root',
+  password: '111111'
+});
+var db = server.use('o2');
+var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.locals.pretty = true;
+app.use('/user', express.static('uploads'));
+app.set('views', './views_orientdb');
+app.set('view engine', 'jade');
+app.get('/upload', function(req, res){
+  res.render('upload');
+});
+app.post('/upload', upload.single('userfile'), function(req, res){
+  res.send('Uploaded : '+req.file.filename);
+});
+app.get('/topic/new', function(req, res){
+  fs.readdir('data', function(err, files){
+    if(err){
+      console.log(err);
+      res.status(500).send('Internal Server Error');
+    }
+    res.render('new', {topics:files});
+  });
+});
+app.get(['/topic', '/topic/:id'], function(req, res){
+  var sql = 'SELECT FROM topic';
+  db.query(sql).then(function(topics){
+    var id = req.params.id;
+    if(id){
+      var sql = 'SELECT FROM topic WHERE @rid=:rid';
+      db.query(sql, {params:{rid:id}}).then(function(topic){
+        res.render('view', {topics:topics, topic:topic[0]});
+      });
+    } else {
+      res.render('view', {topics:topics});
+    }
+  });
+  /*
+  fs.readdir('data', function(err, files){
+    if(err){
+      console.log(err);
+      res.status(500).send('Internal Server Error');
+    }
+    var id = req.params.id;
+    if(id){
+      // id값이 있을 때
+      fs.readFile('data/'+id, 'utf8', function(err, data){
+        if(err){
+          console.log(err);
+          res.status(500).send('Internal Server Error');
+        }
+        res.render('view', {topics:files, title:id, description:data});
+      })
+    } else {
+      // id 값이 없을 때
+      res.render('view', {topics:files, title:'Welcome', description:'Hello, JavaScript for server.'});
+    }
+  })
+  */
+});
+app.post('/topic', function(req, res){
+  var title = req.body.title;
+  var description = req.body.description;
+  fs.writeFile('data/'+title, description, function(err){
+    if(err){
+      console.log(err);
+      res.status(500).send('Internal Server Error');
+    }
+    res.redirect('/topic/'+title);
+  });
+})
+app.listen(3000, function(){
+  console.log('Connected, 3000 port!');
+})
