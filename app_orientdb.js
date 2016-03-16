68c81a2b357f2907583919f7b66df0a53f4f7ecf
egoing <egoing@gmail.com>
2016-03-16 12:14:40 +0900

    데이터베이스 orientdb12 웹앱제작-편집2 예제

--- app_orientdb.js
+++ app_orientdb.js
@@ -34,10 +34,6 @@ app.post('/upload', upload.single('userfile'), function(req, res){
 app.get('/topic/add', function(req, res){
   var sql = 'SELECT FROM topic';
   db.query(sql).then(function(topics){
-    if(topics.length === 0){
-      console.log('There is no topic record.');
-      res.status(500).send('Internal Server Error');
-    }
     res.render('add', {topics:topics});
   });
 });
@@ -56,7 +52,33 @@ app.post('/topic/add', function(req, res){
     res.redirect('/topic/'+encodeURIComponent(results[0]['@rid']));
   });
 });
-
+app.get('/topic/:id/edit', function(req, res){
+  var sql = 'SELECT FROM topic';
+  var id = req.params.id;
+  db.query(sql).then(function(topics){
+    var sql = 'SELECT FROM topic WHERE @rid=:rid';
+    db.query(sql, {params:{rid:id}}).then(function(topic){
+      res.render('edit', {topics:topics, topic:topic[0]});
+    });
+  });
+});
+app.post('/topic/:id/add', function(req, res){
+  var sql = 'UPDATE topic SET title=:t, description=:d, author=:a WHERE @rid=:rid';
+  var id = req.params.id;
+  var title = req.body.title;
+  var desc = req.body.description;
+  var author = req.body.author;
+  db.query(sql, {
+    params:{
+      t:title,
+      d:desc,
+      a:author,
+      rid:id
+    }
+  }).then(function(topics){
+    res.redirect('/topic/'+encodeURIComponent(id));
+  });
+});
 app.get(['/topic', '/topic/:id'], function(req, res){
   var sql = 'SELECT FROM topic';
   db.query(sql).then(function(topics){
