17c1775c5afa1c90efb2f780d62ae0812f2c4eb6
egoing <egoing@gmail.com>
2016-05-10 14:58:35 +0900

    auth+crud for mysql 6

--- routes/mysql/auth.js
+++ routes/mysql/auth.js
@@ -8,7 +8,7 @@ module.exports = function(passport){
     passport.authenticate(
       'local',
       {
-        successRedirect: '/welcome',
+        successRedirect: '/topic',
         failureRedirect: '/auth/login',
         failureFlash: false
       }
@@ -26,7 +26,7 @@ module.exports = function(passport){
     passport.authenticate(
       'facebook',
       {
-        successRedirect: '/welcome',
+        successRedirect: '/topic',
         failureRedirect: '/auth/login'
       }
     )
@@ -56,15 +56,21 @@ module.exports = function(passport){
     });
   });
   route.get('/register', function(req, res){
-    res.render('auth/register');
+    var sql = 'SELECT id,title FROM topic';
+    conn.query(sql, function(err, topics, fields){
+      res.render('auth/register', {topics:topics});
+    });
   });
   route.get('/login', function(req, res){
-    res.render('auth/login');
+    var sql = 'SELECT id,title FROM topic';
+    conn.query(sql, function(err, topics, fields){
+      res.render('auth/login', {topics:topics});
+    });
   });
   route.get('/logout', function(req, res){
     req.logout();
     req.session.save(function(){
-      res.redirect('/welcome');
+      res.redirect('/topic');
     });
   });
   return route;
